b3e32bb003f00f00dcddc28ae03c732dd9461529
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-07-11 10:17:37 -0700

    add model with seeds for initial data

---
+++ db/migrate/20230710190919_create_pantry_items.rb
@@ -0,0 +1,11 @@
+class CreatePantryItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pantry_items do |t|
+      t.integer :ingredient_id
+      t.integer :user_id
+      t.decimal :amount, precision: 5, scale: 2
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ingredient = Ingredient.create!({ name: "spaghetti noodle", image_url: "https://feelgoodfoodie.net/wp-content/uploads/2020/06/how-to-cook-pasta-6.jpg" })
+
+ingredient = Ingredient.create!({ name: "ground beef", image_url: "https://embed.widencdn.net/img/beef/4hh1pywcnj/800x600px/Grind_Fine_85.psd?keep=c&u=7fueml" })
+
+ingredient = Ingredient.create!({ name: "spaghetti sauce", image_url: "https://cookfasteatwell.com/wp-content/uploads/2019/02/Easy-Pasta-Sauce_Quick-Recipe.jpg" })
+
+ingredient = Ingredient.create!({ name: "garlic", image_url: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSXhniwiCZvRWsMUW28qCEtk9lCSvekgLu7a9OMhZlv5eKHwnGG6AYyp6P7k6dDQWUUf0ccjAxJQzprmCA" })
+
+ingredient = Ingredient.create!({ name: "onion", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Onion_on_White.JPG/440px-Onion_on_White.JPG" })
+
+ingredient = Ingredient.create!({ name: "salt", image_url: "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322745_1100-732x549.jpg" })
+
+ingredient = Ingredient.create!({ name: "Italian seasoning", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Italian_Herb_Mix%2C_Penzeys_Spices%2C_Arlington_Heights_MA.jpg/500px-Italian_Herb_Mix%2C_Penzeys_Spices%2C_Arlington_Heights_MA.jpg" })
+
+ingredient = Ingredient.create!({ name: "burger bun", image_url: "https://www.allrecipes.com/thmb/5KL64r13J8BEQtPELARfjrhsEU4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/233652-homemade-hamburger-buns-DDMFS-4x3-d739c2f73b7d4c3b8f68f211d193d1f5.jpg" })
+
+ingredient = Ingredient.create!({ name: "tomato", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Bright_red_tomato_and_cross_section02.jpg/440px-Bright_red_tomato_and_cross_section02.jpg" })
+
+ingredient = Ingredient.create!({ name: "cheeese", image_url: "https://cdn.schwans.com/media/images/products/62172-1-1540.jpg" })
+
+ingredient = Ingredient.create!({ name: "ketchup", image_url: "https://m.media-amazon.com/images/I/51reCAiUlXL._SL1000_.jpg" })
+
+ingredient = Ingredient.create!({ name: "hotdog bun", image_url: "https://www.chopstickchronicles.com/wp-content/uploads/2020/07/Hot-Dog-Buns-Koppepan-41-1.jpg" })
+
+ingredient = Ingredient.create!({ name: "beef hot link", image_url: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS6AD7bbcyzD2Rvs9wjgGN8msKi-uAdP3D2x9B93pMN-h82Wz9-0GxDsVUYZxGYjdZrgCltHUQEBNVBDoe0fUv5Xw7YRW7po9q8hNayC4DOKHzeq0_qc1H5" })
+
+ingredient = Ingredient.create!({ name: "jalapeno", image_url: "https://upload.wikimedia.org/wikipedia/commons/d/d6/Immature_jalapeno_capsicum_annuum_var_annuum.jpeg" })
+
+ingredient = Ingredient.create!({ name: "flanken short ribs", image_url: "https://meatnbone.com/cdn/shop/products/Flanken3.jpg?v=1631221513" })
+
+ingredient = Ingredient.create!({ name: "oyster sauce", image_url: "https://m.media-amazon.com/images/I/71vSvE9CViL._SL1500_.jpg" })
+
+ingredient = Ingredient.create!({ name: "honey", image_url: "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/04/honey-1296x728-header.jpg?w=1155&h=1528" })
+
+ingredient = Ingredient.create!({ name: "seasame oil", image_url: "https://m.media-amazon.com/images/I/71HANc-AJwL._SY879_.jpg" })
+
+ingredient = Ingredient.create!({ name: "lemon grass", image_url: "https://cdn11.bigcommerce.com/s-ti8ztxg4ry/images/stencil/572x712/products/3066/2246/lemon_grass_-003_SS-2022__75874.1654191092.jpg?c=1" })
+
+ingredient = Ingredient.create!({ name: "rice wind vinegar", image_url: "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2Farchive%2F88ecf67e5cab932a284dd61c8022bd3b2384747a" })
+
+ingredient = Ingredient.create!({ name: "ribeye steak", image_url: "https://embed.widencdn.net/img/beef/ng96sbyljl/800x600px/Ribeye%20Steak_Lip-on.psd?keep=c&u=7fueml" })
+
+ingredient = Ingredient.create!({ name: "black pepper", image_url: "hhttps://m.media-amazon.com/images/I/81Ugi9-f7BL._SL1500_.jpg" })
+
+ingredient = Ingredient.create!({ name: "thyme", image_url: "https://i0.wp.com/abundancewholesomefoods.co.za/wp-content/uploads/2019/12/thyme.jpg?fit=1024%2C1024&ssl=1" })
+
+ingredient = Ingredient.create!({ name: "butter", image_url: "https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcS9M1ewyYcPdBnl9gJ8iPk5oVPCjY0Nc1mBnPZJNHocJcQPmQiBVQSXkH2YxT4tBbuG" })
+
+ingredient = Ingredient.create!({ name: "mango", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Mango_4.jpg/440px-Mango_4.jpg" })
+
+ingredient = Ingredient.create!({ name: "lime", image_url: "https://i5.walmartimages.com/asr/12314833-2e54-4739-94a2-7db45b63109d.16ff07e3c111df9be4158853c2e505ef.jpeg" })
+
+ingredient = Ingredient.create!({ name: "cilantro", image_url: "https://asian-veggies.com/cdn/shop/products/cilantro.jpg?v=1626104591" })
+
+ingredient = Ingredient.create!({ name: "cumin", image_url: "https://i5.walmartimages.com/asr/d28c9bb2-fd8b-4fa4-8a00-752e5958be51_1.63375324f57defe2acb46f2ee6fdbd77.jpeg" })
